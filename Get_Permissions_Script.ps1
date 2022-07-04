e50d89f3b6e9a496710fc095a2866a42006e445c
csbot98 <79809608+csbot98@users.noreply.github.com>
2022-07-04 09:18:29 +0200

    Add files via upload

---
+++ Get_Permissions_Script.ps1
@@ -0,0 +1,21 @@
+﻿###########Export Folder Permisson##############
+#Kiexportálja a mappa/mappák felhasználók jogait.
+##
+#Ezeket átírhatod
+$FolderName="\\192.168.91.4\Share\Teszt"
+$CSVPath="$env:APPDATA\perm.csv"
+##
+#
+##Ehhez már ne nyúlj!
+$FolderPath = dir -Directory -Path $FolderName -Recurse -Force
+$Report = @()
+Foreach ($Folder in $FolderPath) {
+    $Acl = Get-Acl -Path $Folder.FullName
+    foreach ($Access in $acl.Access)
+        {
+            $Properties = [ordered]@{'FolderName'=$Folder.FullName;'ADUser'=$Access.IdentityReference;'Permissions'=$Access.FileSystemRights;'Inherited'=$Access.IsInherited}
+            $Report += New-Object -TypeName PSObject -Property $Properties
+        }
+}
+$Report | Export-Csv -path $CSVPath -Encoding UTF8
+Write-Host "A CSV export elkészült: $CSVPath"
